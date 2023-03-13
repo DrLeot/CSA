33b0f30991e913ca55350a28335f60e5a1150db9
Janik Burkart <janik.burkart@stud.hslu.ch>
2023-03-13 11:32:22 +0100

    VL3 Setup deployment to raspberry pi

---
+++ TestPICommunication/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace TestPiCommunication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            for (int i = 0; i < 30; i++)
+            {
+                Console.Write(".");
+                Thread.Sleep(1000);
+            }            
+        }
+    }
+}
